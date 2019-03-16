6327485036e6d04588ca9d17a70cfe5d6e6caa4e
meta-minal-sharma <46737610+meta-minal-sharma@users.noreply.github.com>
2019-03-16 14:01:47 +0530

    Update CountCharacter.java

--- DSAAssignment4/src/question2/CountCharacter.java
+++ DSAAssignment4/src/question2/CountCharacter.java
@@ -2,51 +2,61 @@ package question2;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+
 public class CountCharacter {
 
-	public static final Map<String,Integer> listOfString = new HashMap<String,Integer>();
-	/**
-	 * count the character which is not present in the list
-	 * @param name
-	 * @return count of character
-	 */
-	public int countUniqueCharactar(String name) {
-		Set<Character> uniqueChar = new HashSet<Character>();
-		for(int i = 0;i<name.length();i++) {
-			if(uniqueChar.contains(name.charAt(i)))
-				uniqueChar.remove(name.charAt(i));
-			else
-				uniqueChar.add(name.charAt(i));      
+	HashMap<String, Integer> cache= new HashMap<>();
+
+	public long numberOfUniqueCharacter(String input) {
+		int result=0;
+
+		//firstly checking do we have result for the same string from earlier
+		if(cache.containsKey(input))
+		{
+			result = cache.get(input);
+			return result;
 		}
-		listOfString.put(name, uniqueChar.size());
-		return uniqueChar.size();
 
-	}
-	/**
-	 * count the character on the basis of pre information stored in map
-	 * @param u
-	 * @return
-	 */
+		//for storing character one by one uniquely character is key and value is  of character in that string
+		HashMap<Character, Integer> uniquecharacter= new HashMap<>();
 
-	public int countChar(String u) {
-		int result = 0;
-		String name = u;
-		if(listOfString.get(name) != null) {
-			return listOfString.get(name); }
-		else {
-			for(String value1 : listOfString.keySet()) {
-				CharSequence value=value1;
-				if(name.contains(value)) {
-					result=result+listOfString.get(value1);
-					name=name.replace(value1,"");
-				}
+		for(int i=0;i<input.length();i++)
+		{
+			char tempChar= input.charAt(i);
+
+			//if character is already exist in map then update it's value 
+			if(uniquecharacter.containsKey(tempChar))
+			{
+				int val= uniquecharacter.get(tempChar);
+				uniquecharacter.put(tempChar,val+1);
 			}
-		}
-		return result+countUniqueCharactar(name);
 
+			//if not exist then put in hashMap
+			else
+				uniquecharacter.put(tempChar, 1);
+
+		}
+
+		//creating a set of keySet of HashMap
+		Set<Character> keyset= uniquecharacter.keySet();
+		//creating a iterator for traversing set
+		Iterator<Character> iterator = keyset.iterator(); 
+
+		while(iterator.hasNext())
+		{
+			//if character occurence (stored in map as value) is one then increment result value
+			if(uniquecharacter.get(iterator.next()) == 1 )
+				result ++;
+		}
+		//put result in cash hashMap for future use
+		cache.put(input, result);
+		//return result
+		return result;
 	}
 }
+
